3554ca7c34cbbfe50a8b64cf73d3b7f4b7f2ca74
malesal <133693731+malesal@users.noreply.github.com>
2023-06-26 13:19:47 -0300

    Update tp45.cpp

--- tp45.cpp
+++ tp45.cpp
@@ -3,29 +3,24 @@ using namespace std;
 
 bool EsBisiesto(int anio);
 
-int main(){
-	int a;
-	bool bisiesto;
-	
-	cout << "Ingrese año: ";
-	cin >> a;
-	
-	bisiesto = EsBisiesto(a);
-	
-	if(bisiesto == true){
-		cout << "SI";
-	}else{
-		cout << "NO";
-	}
-	
-	return 0;
+int main() {
+    int a;
+    bool bisiesto;
+
+    cout << "Ingrese aÃ±o: ";
+    cin >> a;
+
+    bisiesto = EsBisiesto(a);
+
+    if (bisiesto) {
+        cout << "SI";
+    } else {
+        cout << "NO";
+    }
+
+    return 0;
 }
 
-bool EsBisiesto(int anio){
-	if((anio % 100 == 0 && anio % 400 == 0) || (anio % 100 != 0 && anio % 4 == 0)){
-		return true;
-	} else {
-		return false;
-	}
+bool EsBisiesto(int anio) {
+    return ((anio % 100 == 0 && anio % 400 == 0) || (anio % 100 != 0 && anio % 4 == 0));
 }
-
